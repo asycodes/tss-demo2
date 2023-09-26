f4658b3fb5bd21f6b30dd4ef1237a4eaeb250189
valudollar <valerie.luxuanhui@gmail.com>
2023-09-26 14:31:36 +0800

    template login set up

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,13 +2,51 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Navburger from "../components/navburger";
-
-export default function Home() {
+import tsslogo from "public/tss.svg";
+export default function Page() {
   const router = useRouter();
 
+  function handleLogin() {
+    router.push("/journey/occupations");
+  }
   return (
     <div>
       <Navburger />
+      <div className="w-full">
+        <Image src={tsslogo} width={140} height={140} className="p-5"></Image>
+      </div>
+      <div className="flex flex-col w-full justify-center items-center">
+        <div className="flex flex-col w-10/12 mt-[2rem]">
+          <p>Log In</p>
+          <input
+            type="text"
+            className="bg-transparent border rounded-md pl-2  pt-2 pb-2 mt-4 mb-2"
+            placeholder="E-mail"
+          ></input>
+          <input
+            type="text"
+            className="bg-transparent border rounded-md pl-2 pt-2 pb-2 mt-2 mb-2"
+            placeholder="Password"
+          ></input>
+          <div className="flex justify-end w-full">
+            <button className="text-xs">Forgot Password?</button>
+          </div>
+
+          <button
+            className="mt-[1rem] p-[1rem] rounded-full bg-[#908F8F] font-bold "
+            onClick={handleLogin}
+          >
+            Log in
+          </button>
+          <button className=" p-[1rem] rounded-full border-white border mt-[1rem]">
+            Don't have an account? <b>Sign Up</b>
+          </button>
+          <p className="text-xs text-center mt-[4rem]">
+            By signing up, you’re agreeing to our
+            <b> Privacy Policy and Terms of Service</b>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
