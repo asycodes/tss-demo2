40c66a906ec8eadbaf3e6603f213515b9ea7cf5a
Asyraf Omar <asyrafomar68@gmail.com>
2023-11-29 10:51:20 +0800

    adjusted edit

--- src/app/journey/occupations/edit/page.jsx
+++ src/app/journey/occupations/edit/page.jsx
@@ -21,11 +21,11 @@ const fetchData = async () =>{
       method: "POST",
       data: json,
     });
-    console.log(res.data.body)
     const jsonData = await JSON.parse(res.data.body);
     return jsonData.flat()
   } catch (error) {
     console.error(error);
+    return []
   }
 }
 
