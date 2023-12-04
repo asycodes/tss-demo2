a69a5fb3c26b4977a5225c68028f644f1e82c0e6
VanBanHan <asyrafomar68@gmail.com>
2023-12-05 07:27:28 +0800

    debug uploadcv

--- src/app/journey/occupations/uploadcv/[id]/page.jsx
+++ src/app/journey/occupations/uploadcv/[id]/page.jsx
@@ -6,30 +6,7 @@ import LoadingPage from "./loading"
 
 //server component
 const url = "https://9hxkxfyhu4.execute-api.ap-southeast-1.amazonaws.com/dev/post-json";
-const dummy = async () => {
-  const url =
-    "https://9hxkxfyhu4.execute-api.ap-southeast-1.amazonaws.com/dev/post-json";
-  try {
-    const json = JSON.stringify({
-      action: "select",
-      table: "resume_sent_job_done",
-      task: "na",
-      iwa: "na",
-      filename: "5.pdf",
-      column: "job_done",
-    });
 
-    const res = await axios(url, {
-      method: "POST",
-      data: json,
-    });
-    const jsonData = await JSON.parse(res.data.body);
-    return jsonData.flat();
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
 
 const checkjob = async (id) =>{
   try {
@@ -50,7 +27,7 @@ const checkjob = async (id) =>{
     return jsonData
   } catch (error) {
     console.error(error);
-    return checkjob(id)
+    return []
   }
 }
 
@@ -87,7 +64,6 @@ const fetchData = async (id) =>{
 }
 
 export default async function Page({params}) {
-  await dummy()
   const id = params.id
   const tasks = await fetchData(id)
   console.log(tasks)
